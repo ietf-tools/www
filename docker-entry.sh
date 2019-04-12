917f204dfb2b9da95c1f10b11ad742aa9f2baec2
Robert Sparks <rjsparks@nostrum.com>
2019-04-12 11:55:00 -0500

    Disallow user prompts when running collectstatic

--- docker-entry.sh
+++ docker-entry.sh
@@ -3,7 +3,7 @@
 trap "echo TRAPed signal" HUP INT QUIT TERM
 
 sudo -E -u wwwrun ./manage.py migrate
-sudo -E -u wwwrun ./manage.py collectstatic
+sudo -E -u wwwrun ./manage.py collectstatic --noinput
 sudo -E -u wwwrun ./manage.py update_index_no_datatracker
 
 ./mod_wsgi-express-8001/apachectl start
