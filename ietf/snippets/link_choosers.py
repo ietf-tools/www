1c461bc5acea3776f71392c54c0f2f44b4287674
Robert Sparks <rjsparks@nostrum.com>
2020-02-26 15:51:37 -0600

    Move link_choosers and wagtail_hooks content out of the datatracker app.

--- ietf/snippets/link_choosers.py
+++ ietf/snippets/link_choosers.py
@@ -1,6 +1,7 @@
 from django.utils.html import escape
+from django.core.exceptions import ObjectDoesNotExist
 
-from ietf.snippets.models import GlossaryItem
+from .models import GlossaryItem, RFC, Charter
 
 
 class LinkChooser(object):
@@ -21,10 +22,13 @@ class RichTextLinkHandler(object):
 
     @classmethod
     def expand_db_attributes(cls, attrs):
-        page = cls.model.objects.get(id=attrs['id'])
-        app = cls.model._meta.app_label
-        editor_attrs = 'data-linktype="{}" data-id="{}" data-app="{}"'.format(cls.link_type, page.id, app)
-        return '<a {} href="{}">'.format(editor_attrs, escape(page.url))
+        try:
+            page = cls.model.objects.get(id=attrs['id'])
+            app = cls.model._meta.app_label
+            editor_attrs = 'data-linktype="{}" data-id="{}" data-app="{}"'.format(cls.link_type, page.id, app)
+            return '<a {} href="{}">'.format(editor_attrs, escape(page.url))
+        except ObjectDoesNotExist:
+            return '<a>'
 
 
 
@@ -48,7 +52,22 @@ class GlossaryItemLinkChooser(LinkChooser):
     link_type = 'glossaryitem'
     model = GlossaryItem
 
-
 class GlossaryItemRichTextLinkHandler(RichTextLinkHandler):
     link_type = 'glossaryitem'
     model = GlossaryItem
+
+class RFCLinkChooser(LinkChooser):
+    link_type = 'rfc'
+    model = RFC
+
+class RFCRichTextLinkHandler(RichTextLinkHandler):
+    link_type = 'rfc'
+    model = RFC
+
+class CharterLinkChooser(LinkChooser):
+    link_type = 'charter'
+    model = Charter
+
+class CharterRichTextLinkHandler(RichTextLinkHandler):
+    link_type = 'charter'
+    model = Charter
--- ietf/snippets/wagtail_hooks.py
+++ ietf/snippets/wagtail_hooks.py
@@ -2,11 +2,22 @@ from django.conf.urls import include, url
 from django.contrib.staticfiles.templatetags.staticfiles import static
 from django.core.urlresolvers import reverse
 from django.utils.html import format_html, format_html_join
-from wagtail.core import hooks
+
+from wagtail.admin.menu import MenuItem
 from wagtail.admin.rich_text import HalloPlugin
+from wagtail.admin.rich_text.converters import editor_html
+from wagtail.core import hooks
 
 from . import admin_urls
-from ietf.datatracker.link_choosers import GlossaryItemLinkChooser
+from .link_choosers import (
+    ExternalRichTextLinkHandler,
+    GlossaryItemLinkChooser,
+    GlossaryItemRichTextLinkHandler,
+    RFCLinkChooser,
+    RFCRichTextLinkHandler,
+#    CharterLinkChooser,
+#    CharterRichTextLinkHandler,
+)
 
 
 @hooks.register('register_admin_urls')
@@ -45,3 +56,37 @@ def register_embed_feature(features):
     )
     features.default_features.append('snippet')
     features.default_features.append('h5')
+
+
+@hooks.register('register_link_chooser')
+def register_glosary_item_link_chooser():
+    return GlossaryItemLinkChooser()
+
+@hooks.register('register_link_chooser')
+def register_rfc_link_chooser():
+    return RFCLinkChooser()
+
+#@hooks.register('register_link_chooser')
+#def register_charter_link_chooser():
+#    return CharterLinkChooser()
+
+
+@hooks.register('register_rich_text_features')
+def register_link_choosers(features):
+    # Register conversion rules for translating between database and hallo.js
+    features.register_converter_rule(
+        'editorhtml', 'snippet', [
+            editor_html.LinkTypeRule(RFCRichTextLinkHandler.link_type, RFCRichTextLinkHandler),
+#            editor_html.LinkTypeRule(CharterRichTextLinkHandler.link_type, CharterRichTextLinkHandler),
+            editor_html.LinkTypeRule(GlossaryItemRichTextLinkHandler.link_type, GlossaryItemRichTextLinkHandler),
+            editor_html.LinkTypeRule(ExternalRichTextLinkHandler.link_type, ExternalRichTextLinkHandler),
+            editor_html.LinkTypeRule('email', ExternalRichTextLinkHandler),
+        ]
+    )
+
+    # Register conversion rules for translating from database to frontend.
+    # Here we re-use the conversion function from hallo.js (which means that the data-foo
+    # attributes also get added, which may or may not be a good thing.)
+    features.register_link_type(GlossaryItemRichTextLinkHandler.link_type, GlossaryItemRichTextLinkHandler.expand_db_attributes)
+    features.register_link_type(RFCRichTextLinkHandler.link_type, RFCRichTextLinkHandler.expand_db_attributes)
+#    features.register_link_type(CharterRichTextLinkHandler.link_type, CharterRichTextLinkHandler.expand_db_attributes)
