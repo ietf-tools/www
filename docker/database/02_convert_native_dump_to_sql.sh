ca024f8a7603c9d93ab3710c4f0d20527bfbc5c6
Eugene Dementiev <eugene@springload.co.nz>
2020-09-28 09:46:54 +1300

    Modify the database converter script to take gzipped
    files as well

--- docker/database/02_convert_native_dump_to_sql.sh
+++ docker/database/02_convert_native_dump_to_sql.sh
@@ -1,16 +1,27 @@
 #!/bin/sh
 
+set -e
+
 cd /docker-entrypoint-initdb.d
 
 FILE=$(ls -1 ietfa.torchbox.* | head)
 if [ -s "${FILE}" ]; then
 	echo "Found ${FILE}, converting..."
-	# no owner, no permissions
-	pg_restore -xO -f dump.sql "$FILE"
-	echo "hotfixing the dump"
+	case $FILE in
+		*.gz)
+		# -xO means restore no owner, no permissions
+		echo "Restoring the gzipped archive..."
+		gzip -d -c $FILE | pg_restore -xO -f dump.sql
+		;;
+		*)
+		echo "Restoring the archive..."
+		pg_restore -xO -f dump.sql "$FILE"
+		;;
+	esac
+	echo "Hotfixing the dump"
 	sed -i -e '/CREATE SCHEMA public/d' dump.sql
 else
-	echo "no dump, failing"
+	echo "No dump, failing"
 	exit 1
 fi
 
