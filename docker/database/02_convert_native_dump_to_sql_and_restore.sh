8e1d904646604c103d85e8a24f1cc22a9d4024fd
Eugene Dementiev <eugene@springload.co.nz>
2020-09-29 14:36:56 +1300

    Fix the db loader script requiring 2 runs before.
    Now it doesn't use temporary files, and just pipes everything into
    Postgres, which works faster.

--- docker/database/02_convert_native_dump_to_sql_and_restore.sh
+++ docker/database/02_convert_native_dump_to_sql_and_restore.sh
@@ -7,22 +7,24 @@ FILE=$(ls -1 ietfa.torchbox.* | head)
 
 set -e
 
+restore_dump() {
+	# use docker_process_sql function from docker-entrypoint.sh in the Postgres container
+	# pg_restore -xO means restore no owner, no permissions
+	pg_restore -xO -f - | sed -e '/CREATE SCHEMA public/d' | docker_process_sql
+}
+
 if [ -s "${FILE}" ]; then
 	echo "Found ${FILE}, converting..."
 	case $FILE in
 		*.gz)
-		# -xO means restore no owner, no permissions
 		echo "Restoring the gzipped archive..."
-		gzip -d -c $FILE | pg_restore -xO -f dump.sql
+		gzip -d -c $FILE | restore_dump
 		;;
 		*)
 		echo "Restoring the archive..."
-		pg_restore -xO -f dump.sql "$FILE"
+		cat $FILE | restore_dump
 		;;
 	esac
-	# schema public is already created
-	echo "Hotfixing the dump"
-	sed -i -e '/CREATE SCHEMA public/d' dump.sql
 else
 	echo "No dump, starting fresh."
 fi
