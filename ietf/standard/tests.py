90415dd549dd62fa9e48718362ea50c8e54ed2a8
Lydie Danet <lydie.danet@springload.co.nz>
2020-10-13 15:35:55 +1300

    Add basic wagtail test for standard page

---
+++ ietf/standard/tests.py
@@ -0,0 +1,56 @@
+from django.test import TestCase
+
+from .models import StandardPage, StandardIndexPage
+from ..home.models import HomePage
+
+from wagtail.core.models import Page, Site
+
+class StandardPageTests(TestCase):
+
+    def test_standard_page(self):
+
+        root = Page.get_first_root_node()
+
+        home = HomePage(
+            slug = 'homepageslug',
+            title = 'home page title',
+            heading = 'home page heading',
+            introduction = 'home page introduction',
+            request_for_comments_section_body = 'rfc section body',
+            working_groups_section_body = 'wg section body',
+        )
+
+        root.add_child(instance=home)
+
+        Site.objects.all().delete()
+
+        Site.objects.create(
+            hostname='localhost',
+            root_page = home,
+            is_default_site=True,
+            site_name='testingsitename',
+        )
+
+        standardindex = StandardIndexPage(
+            slug = 'standardindex',
+            title = 'standard index page title',
+            introduction = 'standard index page introduction'
+        )
+        home.add_child(instance = standardindex)       
+
+        standardpage = StandardPage(
+            slug = 'standard',
+            title = 'standard title',
+            introduction = 'standard introduction',
+        )
+        standardindex.add_child(instance = standardpage)
+
+        rindex = self.client.get(path=standardindex.url or '/standardindex')
+        self.assertEqual(rindex.status_code, 200)
+
+        r = self.client.get(path=standardpage.url or '/standardindex/standard')
+        self.assertEqual(r.status_code, 200)
+
+        self.assertIn(standardpage.title.encode(), r.content)
+        self.assertIn(standardpage.introduction.encode(), r.content)
+        self.assertIn(('href="%s"' % standardindex.url).encode(), r.content)
