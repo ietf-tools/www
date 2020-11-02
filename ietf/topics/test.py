565039e8ece78d29844b946eede1d7f22effd906
Lydie Danet <lydie.danet@springload.co.nz>
2020-11-03 10:11:02 +1300

    Add basic wagtail test for topic index and topic page

---
+++ ietf/topics/test.py
@@ -0,0 +1,56 @@
+from django.test import TestCase
+
+from .models import TopicIndexPage, PrimaryTopicPage
+from ..home.models import HomePage
+
+from wagtail.core.models import Page, Site
+
+class StandardPageTests(TestCase):
+
+    def test_standard_page(self):
+
+        root = Page.get_first_root_node()
+
+        home = HomePage(
+            slug = 'homepageslug',
+            title = 'home page title',
+            heading = 'home page heading',
+            introduction = 'home page introduction',
+            request_for_comments_section_body = 'rfc section body',
+            working_groups_section_body = 'wg section body',
+        )
+
+        root.add_child(instance=home)
+
+        Site.objects.all().delete()
+
+        Site.objects.create(
+            hostname='localhost',
+            root_page = home,
+            is_default_site=True,
+            site_name='testingsitename',
+        )
+
+        topicindex = TopicIndexPage(
+            slug = 'topicindex',
+            title = 'topic index page title',
+            introduction = 'topic index page introduction'
+        )
+        home.add_child(instance = topicindex)       
+
+        topicpage = PrimaryTopicPage(
+            slug = 'topic',
+            title = 'topic title',
+            introduction = 'topic introduction',
+        )
+        topicindex.add_child(instance = topicpage)
+
+        rindex = self.client.get(path=topicindex.url or '/topicindex')
+        self.assertEqual(rindex.status_code, 200)
+
+        r = self.client.get(path=topicpage.url or '/topicindex/topic')
+        self.assertEqual(r.status_code, 200)
+
+        self.assertIn(topicpage.title.encode(), r.content)
+        self.assertIn(topicpage.introduction.encode(), r.content)
+        self.assertIn(('href="%s"' % topicindex.url).encode(), r.content)
