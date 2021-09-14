9e9f552f22d8ab945554303341e0ece5c37beaad
Lars Eggert <lars@eggert.org>
2021-09-14 13:24:41 +0300

    Add wagtailmarkdown. Fixes #123.

--- ietf/utils/blocks.py
+++ ietf/utils/blocks.py
@@ -4,12 +4,14 @@ from wagtail.core.blocks import (
 from wagtail.contrib.table_block.blocks import TableBlock
 from wagtail.images.blocks import ImageChooserBlock
 from wagtail.embeds.blocks import EmbedBlock
+from wagtailmarkdown.blocks import MarkdownBlock
 
 
 class StandardBlock(StreamBlock):
     heading = CharBlock(icon="title")
     paragraph = RichTextBlock(icon="pilcrow")
     image = ImageChooserBlock(icon="image", template='includes/imageblock.html')
+    markdown = MarkdownBlock(icon="code")
     embed = EmbedBlock(icon="code")
     raw_html = RawHTMLBlock(icon="placeholder")
     table = TableBlock(table_options={'renderer': 'html'}, template="includes/tableblock.html")
