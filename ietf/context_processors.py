29745ca5dea3409b0fc01edf5efb00397fc4ea69
Edward Williams <edward.williams@springload.co.nz>
2021-11-05 13:48:41 +1300

    Fixes tests

--- ietf/context_processors.py
+++ ietf/context_processors.py
@@ -8,8 +8,10 @@ from ietf.utils.models import MenuItem
 def home_page():
     return HomePage.objects.filter(depth=2).first()
 
+
 def children(item):
     return item and item.get_children().live().in_menu()
+
 
 def menu():
     items = children(home_page())
@@ -18,18 +20,24 @@ def menu():
             item.subitems = children(item)
     return items
 
+
 def secondary_menu():
-    items = MenuItem.objects.order_by('sort_order').all().select_related('page').prefetch_related('sub_menu_items')
+    items = (
+        MenuItem.objects.order_by("sort_order")
+        .all()
+        .select_related("page")
+        .prefetch_related("sub_menu_items")
+    )
     return items
 
 
 def global_pages(request):
     return {
-        'HOME': home_page(),
-        'BLOG_INDEX': BlogIndexPage.objects.first(),
-        'MENU': menu(),
-        'SECONDARY_MENU': secondary_menu,
-        'BASE_URL': getattr(settings, 'BASE_URL', ""),
-        'DEBUG': getattr(settings, 'DEBUG', ""),
-        'FB_APP_ID': getattr(settings, 'FB_APP_ID', ""),
+        "HOME": home_page(),
+        "BLOG_INDEX": BlogIndexPage.objects.first(),
+        "MENU": menu(),
+        "SECONDARY_MENU": secondary_menu(),
+        "BASE_URL": getattr(settings, "BASE_URL", ""),
+        "DEBUG": getattr(settings, "DEBUG", ""),
+        "FB_APP_ID": getattr(settings, "FB_APP_ID", ""),
     }
