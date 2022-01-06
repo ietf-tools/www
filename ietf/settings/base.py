46fdf290a1b836a17784492f84366a28b8b242b6
Kesara Rathnayake <kesara@fq.nz>
2022-01-07 10:36:32 +1300

    Enable TypedTable block

--- ietf/settings/base.py
+++ ietf/settings/base.py
@@ -75,6 +75,7 @@ INSTALLED_APPS = (
     "analytical",
     "wagtailmarkdown",
     "wagtailorderable",
+    "wagtail.contrib.typed_table_block",
 )
 
 MIDDLEWARE = [
--- ietf/utils/blocks.py
+++ ietf/utils/blocks.py
@@ -2,6 +2,8 @@ from wagtail.core.blocks import (
     CharBlock, RichTextBlock, StreamBlock, RawHTMLBlock
 )
 from wagtail.contrib.table_block.blocks import TableBlock
+from wagtail.contrib.typed_table_block.blocks import TypedTableBlock
+from wagtail.core import blocks
 from wagtail.images.blocks import ImageChooserBlock
 from wagtail.embeds.blocks import EmbedBlock
 from wagtailmarkdown.blocks import MarkdownBlock
@@ -15,3 +17,9 @@ class StandardBlock(StreamBlock):
     embed = EmbedBlock(icon="code")
     raw_html = RawHTMLBlock(icon="placeholder")
     table = TableBlock(table_options={'renderer': 'html'}, template="includes/tableblock.html")
+    typed_table = TypedTableBlock([
+        ('text', blocks.CharBlock()),
+        ('numeric', blocks.FloatBlock()),
+        ('rich_text', blocks.RichTextBlock()),
+        ('image', image)
+    ])
