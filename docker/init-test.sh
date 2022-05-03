79b214c4a61ad407e6bb4cc5a568bd8b2524325a
Kesara Rathnayake <kesara@fq.nz>
2022-05-03 21:38:48 +1200

    test: Check for missing migrations (#185)

--- docker/init-test.sh
+++ docker/init-test.sh
@@ -4,3 +4,4 @@ python /app/manage.py migrate --no-input
 python /app/manage.py createcachetable
 python /app/manage.py collectstatic
 python /app/manage.py test
+python /app/manage.py makemigrations --check --no-input
