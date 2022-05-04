51ab1325b652f38cbcf84c82a888b174f7e77a61
Kesara Rathnayake <kesara@fq.nz>
2022-05-04 12:50:28 +1200

    test: Add dry run option to missing migration check (#187)

--- docker/init-test.sh
+++ docker/init-test.sh
@@ -4,4 +4,4 @@ python /app/manage.py migrate --no-input
 python /app/manage.py createcachetable
 python /app/manage.py collectstatic
 python /app/manage.py test
-python /app/manage.py makemigrations --check --no-input
+python /app/manage.py makemigrations --check --dry-run --no-input
