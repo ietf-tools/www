09d77aa8dbdfa93765de10645dfabb0c2303ff8c
removedporn <86824510+removedporn@users.noreply.github.com>
2022-07-06 17:08:20 +0800

    Delete post-deploy.sh

--- post-deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-sudo --preserve-env=PATH,VIRTUAL_ENV,DJANGO_SETTINGS_MODULE -u wwwrun ./manage.py migrate
-sudo --preserve-env=PATH,VIRTUAL_ENV,DJANGO_SETTINGS_MODULE -u wwwrun ./manage.py collectstatic --noinput
-sudo --preserve-env=PATH,VIRTUAL_ENV,DJANGO_SETTINGS_MODULE -u wwwrun ./manage.py compress
-sudo --preserve-env=PATH,VIRTUAL_ENV,DJANGO_SETTINGS_MODULE -u wwwrun ./manage.py update_index
