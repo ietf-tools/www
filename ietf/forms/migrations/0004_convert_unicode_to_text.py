2c511dd794847c747844cc1669c65e6a1d8b7f05
Sophie He <sophie.he@springload.co.nz>
2022-07-22 16:10:57 +1200

    Add the check before run the migration

--- ietf/forms/migrations/0004_convert_unicode_to_text.py
+++ ietf/forms/migrations/0004_convert_unicode_to_text.py
@@ -2,10 +2,11 @@
 from django.db import migrations, models
 
 def convert_unicode_to_text(apps, schema_editor):
-    Submissions = apps.get_model('wagtailforms', 'formsubmission')
-    for submission in Submissions.objects.all():
-        submission.form_data = str(submission.form_data.replace('\\u0000',''))
-        submission.save()
+    if apps.is_installed('wagtailforms'):
+        Submissions = apps.get_model('wagtailforms', 'formsubmission')
+        for submission in Submissions.objects.all():
+            submission.form_data = str(submission.form_data.replace('\\u0000',''))
+            submission.save()
 
 
 class Migration(migrations.Migration):
