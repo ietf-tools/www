d25424642e42a94d7837f5dddaf79c0046cc6965
Andrew Calder <andrew.calder@springload.co.nz>
2023-02-02 10:48:12 +1300

    Make menu return correct type of Home Page & menu options generated from content tree.

--- ietf/context_processors.py
+++ ietf/context_processors.py
@@ -1,11 +1,14 @@
 from django.conf import settings
+from wagtail.models import Site
 
 from ietf.blog.models import BlogIndexPage
-from ietf.home.models import HomePage
+from ietf.home.models import HomePage, IABHomePage
 from ietf.utils.models import MenuItem
 
 
-def home_page():
+def home_page(site):
+    if "iab" in site.hostname:
+        return IABHomePage.objects.filter(depth=2).first()
     return HomePage.objects.filter(depth=2).first()
 
 
@@ -13,15 +16,15 @@ def children(item):
     return item and item.get_children().live().in_menu()
 
 
-def menu():
-    items = children(home_page())
+def menu(site):
+    items = children(home_page(site))
     if items:
         for item in items:
             item.subitems = children(item)
     return items
 
 
-def secondary_menu():
+def secondary_menu(site):
     items = (
         MenuItem.objects.order_by("sort_order")
         .all()
@@ -32,11 +35,12 @@ def secondary_menu():
 
 
 def global_pages(request):
+    site = Site.find_for_request(request)
     return {
-        "HOME": home_page(),
+        "HOME": home_page(site),
         "BLOG_INDEX": BlogIndexPage.objects.first(),
-        "MENU": menu(),
-        "SECONDARY_MENU": secondary_menu(),
+        "MENU": menu(site),
+        "SECONDARY_MENU": secondary_menu(site),
         "BASE_URL": getattr(settings, "WAGTAILADMIN_BASE_URL", ""),
         "DEBUG": getattr(settings, "DEBUG", ""),
         "FB_APP_ID": getattr(settings, "FB_APP_ID", ""),
