4f0d27f1409ec2542d62a67a423128e281b2c3b6
Andrew Calder <andrew.calder@springload.co.nz>
2023-02-13 11:26:10 +1300

    Add management command to convert select production website addresses to non-prod addresses.

---
+++ ietf/utils/management/commands/update_nonprod_hostnames.py
@@ -0,0 +1,44 @@
+import os
+from logging import Logger
+
+from django.core.management.base import BaseCommand
+from wagtail.models import Site
+
+logger = Logger(__name__)
+
+
+ALLOWED_ENVIRONMENTS = [
+    "preview",
+    "staging",
+]
+
+HOSTNAME_TRANSLATIONS = {
+    "www.ietf.org": {
+        "preview": "wwwdev.ietf.org",
+        "staging": "wwwstaging.ietf.org",
+    },
+    "www.iab.org": {
+        "preview": "iabdev.ietf.org",
+        "staging": "iabstaging.ietf.org",
+    },
+}
+
+
+class Command(BaseCommand):
+    help = "Updates Site objects to have appropriate hostnames for select non-production environments"
+
+    def handle(self, *args, **options):
+        # First, make sure we're on the appropriate enviornment.
+        environment = os.getenv("ENVIRONMENT")
+        if environment not in ALLOWED_ENVIRONMENTS:
+            logger.warning("This command is not allowed for this environment.")
+
+        # Now adjust hostnames
+        all_sites = Site.objects.all()
+        for site in all_sites:
+            if site.hostname not in HOSTNAME_TRANSLATIONS:
+                logger.warning("Site hostname in database is not able to be updated.")
+                continue
+            new_hostname = HOSTNAME_TRANSLATIONS[site.hostname][environment]
+            site.hostname = new_hostname
+            site.save()
