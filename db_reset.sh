9f38d34b598f69ca52eccb92840c301cd43df1fb
Kesara Rathnayake <kesara@fq.nz>
2023-02-24 15:37:16 +1300

    chore: Disable db sync in wwwdev (temporary change) (#261)

--- db_reset.sh
+++ db_reset.sh
@@ -12,10 +12,10 @@ echo "Getting backups from ${BACKUPS_BUCKET}"
 # 4. Insert drop schema to the beginning
 # 5. Run the commands in a single transaction and exit on error
 # So if anything of the above fails, the database should be left untouched
-aws s3 cp s3://${BACKUPS_BUCKET}/ietfa.torchbox.gz - | gzip -d -c | pg_restore -xO -f - | sed "1s/^/DROP SCHEMA public CASCADE; CREATE SCHEMA public;/" | psql "$DATABASE_URL" -v ON_ERROR_STOP=1 --single-transaction -f -
+#aws s3 cp s3://${BACKUPS_BUCKET}/ietfa.torchbox.gz - | gzip -d -c | pg_restore -xO -f - | sed "1s/^/DROP SCHEMA public CASCADE; CREATE SCHEMA public;/" | psql "$DATABASE_URL" -v ON_ERROR_STOP=1 --single-transaction -f -
 
-python manage.py migrate --noinput
+#python manage.py migrate --noinput
 
-aws s3 sync --delete s3://${BACKUPS_BUCKET}/media/ s3://${AWS_STORAGE_BUCKET_NAME}/media/
+#aws s3 sync --delete s3://${BACKUPS_BUCKET}/media/ s3://${AWS_STORAGE_BUCKET_NAME}/media/
 
-python manage.py update_nonprod_hostnames
+#python manage.py update_nonprod_hostnames
