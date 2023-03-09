f6721db3aba9b731b576fdbc646da68db6bd54c8
Andrew Calder <andrew.calder@springload.co.nz>
2023-03-09 16:13:19 +1300

    Revert "Make disclaimer page grab Note Well text from Note Well page"
    This reverts commit a4cffeb671c1151fa637824585609824b4bb62c4.

--- ietf/snippets/views.py
+++ ietf/snippets/views.py
@@ -1,16 +1,12 @@
-from django.shortcuts import get_object_or_404, render
 
-from ietf.standard.models import StandardPage
+from django.shortcuts import get_object_or_404, render
 
 from .models import MailingListSignup
 
 
 def disclaimer(request, signup_id):
     signup = get_object_or_404(MailingListSignup, pk=signup_id)
-    note_well = StandardPage.objects.filter(slug="note-well").first()
+    return render(request, "snippets/disclaimer.html", {'url': signup.link})
 
-    return render(
-        request,
-        "snippets/disclaimer.html",
-        {"url": signup.link, "note_well": note_well},
-    )
+
+
