70c44bff898b21efefe0ea9943fb7b4d5d16cd6d
Andrew Calder <92233792+AndrewCalderSpringload@users.noreply.github.com>
2023-03-10 14:42:21 +1300

    feat: Note well enhancements (#267)
    * Make disclaimer page grab Note Well text from Note Well page
    
    * Add management command to auto update note well
    
    * Make note well manage command run daily
    
    * Make update_note_well manage command use a setting, change note well repo URL
    
    * Update note well error text

--- ietf/snippets/views.py
+++ ietf/snippets/views.py
@@ -1,12 +1,16 @@
-
 from django.shortcuts import get_object_or_404, render
+
+from ietf.standard.models import StandardPage
 
 from .models import MailingListSignup
 
 
 def disclaimer(request, signup_id):
     signup = get_object_or_404(MailingListSignup, pk=signup_id)
-    return render(request, "snippets/disclaimer.html", {'url': signup.link})
+    note_well = StandardPage.objects.filter(slug="note-well").first()
 
-
-
+    return render(
+        request,
+        "snippets/disclaimer.html",
+        {"url": signup.link, "note_well": note_well},
+    )
