f247ff3b968919359f917bfd4e76bd2fc30ef6c1
Andrew Calder <andrew.calder@springload.co.nz>
2023-03-16 12:12:09 +1300

    Remove cruft/simplify announcements, add migrations

--- ietf/announcements/models.py
+++ ietf/announcements/models.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 from django.db import models
 from wagtail.admin.panels import FieldPanel
-from wagtail.fields import RichTextField, StreamField
+from wagtail.fields import StreamField
 from wagtail.models import Page
 from wagtail.search import index
 
@@ -22,7 +22,12 @@ class IABAnnouncementPage(Page):
     subpage_types = []
 
     date = models.DateTimeField()
-    introduction = RichTextField()
+    introduction = models.CharField(
+        blank=True,
+        max_length=255,
+        help_text="Enter the title to display on the page, "
+        "you can use only 255 characters.",
+    )
     body = StreamField(StandardBlock(), use_json_field=True)
 
     search_fields = Page.search_fields + [
@@ -55,17 +60,9 @@ class IABAnnouncementIndexPage(Page):
         help_text="Enter the title to display on the page, "
         "you can use only 255 characters.",
     )
-    key_info = StreamField(
-        StandardBlock(required=False), blank=True, use_json_field=True
-    )
-    in_depth = StreamField(
-        StandardBlock(required=False), blank=True, use_json_field=True
-    )
 
     search_fields = Page.search_fields + [
         index.SearchField("introduction"),
-        index.SearchField("key_info"),
-        index.SearchField("in_depth"),
     ]
 
     @property
@@ -75,6 +72,4 @@ class IABAnnouncementIndexPage(Page):
 
 IABAnnouncementIndexPage.content_panels = Page.content_panels + [
     FieldPanel("introduction"),
-    FieldPanel("key_info"),
-    FieldPanel("in_depth"),
 ]
