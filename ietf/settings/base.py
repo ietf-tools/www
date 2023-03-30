ff20472b1628b5ee4b98cd7696d4aa71910e6e48
Kesara Rathnayake <kesara@fq.nz>
2023-03-30 14:02:40 +0900

    chore: Use test repostiroy for note well for testing
    This is a temporary change.

--- ietf/settings/base.py
+++ ietf/settings/base.py
@@ -244,4 +244,4 @@ IAB_PARENT_PAGE_TYPES = IAB_SUBPAGE_TYPES + [
     "home.IABHomePage",
 ]
 
-NOTE_WELL_REPO = "https://github.com/ietf/note-well/raw/main/note-well.md"
+NOTE_WELL_REPO = "https://github.com/kesara/note-well/raw/main/note-well.md"
