42a9497575ee24a3692cd384f1a79b85dc32928a
Andrew Calder <andrew.calder@springload.co.nz>
2023-04-14 11:44:35 +1200

    Make disclaimer page use note well TextChunk instead of wagtail page.

--- ietf/snippets/views.py
+++ ietf/snippets/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import get_object_or_404, render
 
-from ietf.standard.models import StandardPage
+from ietf.utils.models import TextChunk
 
 from .models import MailingListSignup
 
 
 def disclaimer(request, signup_id):
     signup = get_object_or_404(MailingListSignup, pk=signup_id)
-    note_well = StandardPage.objects.filter(slug="note-well").first()
+    note_well, created = TextChunk.objects.get_or_create(slug="note-well")
 
     return render(
         request,
