ff985f303b6a0eb8066e9147e141b0d35589330b
Andrew Calder <andrew.calder@springload.co.nz>
2023-04-14 13:58:42 +1200

    Update note well block label

--- ietf/utils/blocks.py
+++ ietf/utils/blocks.py
@@ -44,4 +44,4 @@ class StandardBlock(StreamBlock):
             ("image", ImageChooserBlock()),
         ]
     )
-    note_well = NoteWellBlock(icon="placeholder")
+    note_well = NoteWellBlock(icon="placeholder", label="Note Well Text")
