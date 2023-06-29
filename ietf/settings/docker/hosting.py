2bb8467a280b480280eb0807be93d857cb3cb16e
Kesara Rathnayake <kesara@fq.nz>
2023-06-29 16:52:38 +1200

    chore: Remove leftover AWS configuration (#316)

--- ietf/settings/docker/hosting.py
+++
@@ -1,6 +0,0 @@
-from .base import *
-
-from .grains.aws import *
-
-SECURE_HSTS_SECONDS = 31536000
-CACHE_MIDDLEWARE_ALIAS = "dummy" # Remove after upgrade to Wagtail >= 2.10. See https://github.com/wagtail/wagtail/issues/5975
