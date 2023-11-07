2c276c6b45f5d47dec9137b15cdad1a60bd61e04
Alex Morega <alex.morega@torchbox.com>
2023-11-07 16:46:57 +0200

    Generate XML coverage

--- docker/init-test.sh
+++ docker/init-test.sh
@@ -3,6 +3,6 @@
 python /app/manage.py migrate --no-input
 python /app/manage.py createcachetable
 python /app/manage.py collectstatic --no-input
-pytest --cov
-if [ -d /coverage ]; then cp .coverage /coverage/; fi
+pytest --cov --cov-report=xml
+if [ -d /coverage ]; then cp .coverage coverage.xml /coverage/; fi
 python /app/manage.py makemigrations --check --dry-run --no-input
