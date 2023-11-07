87fb286589ea54887b2e89b396a172b0430ef5bd
Alex Morega <alex.morega@torchbox.com>
2023-11-07 16:35:19 +0200

    Send coverage report to Codecov

--- docker/init-test.sh
+++ docker/init-test.sh
@@ -4,4 +4,5 @@ python /app/manage.py migrate --no-input
 python /app/manage.py createcachetable
 python /app/manage.py collectstatic --no-input
 pytest --cov
+if [ -d /coverage ]; then cp .coverage /coverage/; fi
 python /app/manage.py makemigrations --check --dry-run --no-input
