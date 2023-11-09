07494e5ac71fc74a654507605d8a985a368b90c9
Alex Morega <alex@grep.ro>
2023-11-09 10:58:16 +0200

    feat: Upload coverage reports to Codecov (#340)
    * Send coverage report to Codecov
    
    * Generate XML coverage
    
    ---------
    
    Co-authored-by: Alex Morega <alex.morega@torchbox.com>

--- docker/init-test.sh
+++ docker/init-test.sh
@@ -3,5 +3,6 @@
 python /app/manage.py migrate --no-input
 python /app/manage.py createcachetable
 python /app/manage.py collectstatic --no-input
-pytest --cov
+pytest --cov --cov-report=xml
+if [ -d /coverage ]; then cp .coverage coverage.xml /coverage/; fi
 python /app/manage.py makemigrations --check --dry-run --no-input
