b333b22bed580e26e0bb88405f118acc78013856
Alex Morega <alex@grep.ro>
2023-11-29 23:45:50 +0200

    refactor: Remove dead code (#343)
    Co-authored-by: Alex Morega <alex.morega@torchbox.com>

--- ietf/wsgi.py
+++ ietf/wsgi.py
@@ -15,24 +15,6 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ietf.settings.production")
 
 
 def application(environ, start_response):
-    # This block was from an earlier attempt to manage config via environment variables
-    #try:
-    #    os.environ['DJANGO_SETTINGS_MODULE'] = environ['DJANGO_SETTINGS_MODULE']
-    #    os.environ['CFG_SECRET_KEY'] = environ['CFG_SECRET_KEY']
-    #    os.environ['CFG_ALLOWED_HOSTS'] = environ['CFG_ALLOWED_HOSTS']
-    #    os.environ['CFG_STATIC_URL'] = environ['CFG_STATIC_URL']
-    #    os.environ['CFG_STATIC_DIR'] = environ['CFG_STATIC_DIR']
-    #    os.environ['CFG_MEDIA_URL'] = environ['CFG_MEDIA_URL']
-    #    os.environ['CFG_MEDIA_DIR'] = environ['CFG_MEDIA_DIR']
-    #    os.environ['CFG_APP_NAME'] = environ['CFG_APP_NAME']
-    #    os.environ['PGUSER'] = environ['PGUSER']
-    #    os.environ['PGHOST'] = environ['PGHOST']
-    #    os.environ['PGDATABASE'] = environ['PGDATABASE']
-    #    os.environ['CFG_SERVER_EMAIL'] = environ['CFG_SERVER_EMAIL']
-    #    os.environ['CFG_ERROR_LOG'] = environ['CFG_ERROR_LOG']
-    #except KeyError as e:
-    #    print("At least one environment variable not set: %s" % e)
-
     django_application = get_wsgi_application()
 
     return django_application(environ, start_response)
