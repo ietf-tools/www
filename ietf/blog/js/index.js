5f43f47356d62b56b83acf4c0941acbd58478c8c
Alex Morega <alex.morega@torchbox.com>
2023-12-18 00:16:57 +0200

    chore: Replace Ionicons with Bootstrap Icons (#362)
    * Add bootstrap-icons
    
    * Replace Ionicons with Bootstrap icons
    
    * Remove ionicons library
    
    * Fix mobile search button margin
    
    ---------
    
    Co-authored-by: Kesara Rathnayake <kesara@fq.nz>

--- ietf/blog/js/index.js
+++ ietf/blog/js/index.js
@@ -2,3 +2,4 @@ $('#date_from-input').datepicker();
 $('#date_to-input').datepicker();
 $('#date_from-desktop-input').datepicker();
 $('#date_to-desktop-input').datepicker();
+$('.ui-datepicker-trigger').html('<i class="bi bi-calendar-range"></i>');
