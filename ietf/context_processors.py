f842b3ae1e216a05f05abc82d99cbfa4e50c2c8b
Alex Morega <alex.morega@torchbox.com>
2023-12-18 11:56:43 +0200

    Remove unused context vars

--- ietf/context_processors.py
+++ ietf/context_processors.py
@@ -1,8 +1,7 @@
 from operator import itemgetter
-from django.conf import settings
+
 from wagtail.models import Site
 
-from ietf.blog.models import BlogIndexPage
 from ietf.home.models import HomePage, IABHomePage
 from ietf.utils.models import MenuItem, SocialMediaSettings
 
@@ -53,12 +52,7 @@ def global_pages(request):
     site = Site.find_for_request(request)
     return {
         "HOME": home_page(site),
-        "BLOG_INDEX": BlogIndexPage.objects.first(),
         "MENU": menu(site),
         "SECONDARY_MENU": secondary_menu(site),
         "SOCIAL_MENU": social_menu(site),
-        "BASE_URL": getattr(settings, "WAGTAILADMIN_BASE_URL", ""),
-        "DEBUG": getattr(settings, "DEBUG", ""),
-        "FB_APP_ID": getattr(settings, "FB_APP_ID", ""),
-        "SITE": site,
     }
