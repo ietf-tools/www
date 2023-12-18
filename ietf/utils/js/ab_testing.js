e4c2af3d3a34805fae01781008c143869c019d77
Alex Morega <alex.morega@torchbox.com>
2023-12-18 16:23:13 +0200

    Custom goals

---
+++ ietf/utils/js/ab_testing.js
@@ -0,0 +1,11 @@
+document.querySelector("html").addEventListener("click", (e) => {
+    if (e.target.tagName == "A") {
+        const href = e.target.getAttribute("href") || ""
+        if (href.startsWith("https://www.ietf.org/mailman/listinfo/")) {
+            wagtailAbTesting.triggerEvent("navigate-mailman")
+        }
+        if (href.startsWith("https://registration.ietf.org")) {
+            wagtailAbTesting.triggerEvent("navigate-registration")
+        }
+    }
+})
