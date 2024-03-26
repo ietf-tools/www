d27d164a081dada7c9161607875b04469d715561
Alex Morega <alex.morega@torchbox.com>
2024-03-26 12:19:56 +0200

    fix: Print missing migrations in CI
    https://stackoverflow.com/a/77259833/51801

--- docker/init-test.sh
+++ docker/init-test.sh
@@ -1,8 +1,11 @@
 #!/bin/sh -e
 
+set -x
+
 python /app/manage.py migrate --no-input
 python /app/manage.py createcachetable
 python /app/manage.py collectstatic --no-input
 pytest --cov --cov-report=xml
 if [ -d /coverage ]; then cp .coverage coverage.xml /coverage/; fi
-python /app/manage.py makemigrations --check --dry-run --no-input
+python /app/manage.py makemigrations --dry-run --no-input
+python /app/manage.py makemigrations --check --no-input
