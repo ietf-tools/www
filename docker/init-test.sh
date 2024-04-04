8c34a78625953c91777d149f37c81f94bb773232
Alex Morega <alex.morega@torchbox.com>
2024-04-04 14:41:50 +0300

    Add black and ruff; configure pre-commit

--- docker/init-test.sh
+++ docker/init-test.sh
@@ -2,10 +2,11 @@
 
 set -x
 
+pre-commit run --all-files
+python /app/manage.py makemigrations --dry-run --no-input
+python /app/manage.py makemigrations --check --no-input
 python /app/manage.py migrate --no-input
 python /app/manage.py createcachetable
 python /app/manage.py collectstatic --no-input
 pytest --cov --cov-report=xml
 if [ -d /coverage ]; then cp .coverage coverage.xml /coverage/; fi
-python /app/manage.py makemigrations --dry-run --no-input
-python /app/manage.py makemigrations --check --no-input
