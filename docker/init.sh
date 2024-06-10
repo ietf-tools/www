c62b4126fdb7e68129c8c7200c466c3148cecbc1
Kesara Rathnayake <kesara@fq.nz>
2024-06-11 09:25:35 +1200

    build: Run collectstatic command

--- docker/init.sh
+++ docker/init.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 echo "Running migrate..."
-python /app/manage.py migrate
+python /app/manage.py migrate --no-input
+
+echo "Running collect static..."
+python /app/manage.py collectstatic --no-input
 
 echo "Starting supervisor..."
 /usr/bin/supervisord -c /app/supervisord.conf
